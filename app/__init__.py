ba72252e836181090180ae24eb33f784b327f6b8
amanduhhhh <amandaxi2024@gmail.com>
2026-06-19 14:37:48 -0400

    feat: add base template and dynamic navigation menu

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+pages = [
+    {"name": "Home", "endpoint": "index"},
+    {"name": "Hobbies", "endpoint": "hobbies"},
+]
+
+
+@app.context_processor
+def inject_pages():
+    """Make the nav page list available to every template."""
+    return {"pages": pages}
+
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
