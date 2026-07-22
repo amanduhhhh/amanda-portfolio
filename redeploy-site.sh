881efcaefc15d23fcd73f978a3c9e681a4d70da0
amanduhhhh <amandaxi2024@gmail.com>
2026-07-22 14:35:36 -0400

    fix: redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,6 @@ cd ~/amanda-portfolio
 # make sure the VPS repo matches the latest main from GitHub
 git fetch && git reset origin/main --hard
 
-# enter the virtual environment and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-# restart the service to pick up the changes
-systemctl restart myportfolio.service
+docker compose -f docker-compose.prod.yml up -d --build
